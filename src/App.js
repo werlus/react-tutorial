4b86c07d9c0731a4b42c841ff5cc70662c55e83e
Justin <jsaranovic@gmail.com>
2021-11-30 22:53:01 -0500

    Created character remove function, linked props

--- src/App.js
+++ src/App.js
@@ -1,12 +1,46 @@
 import React, {Component} from 'react'
+import Table from './Table'
 
 class App extends Component {
+    state = {
+        characters: [
+            {
+                name: 'Charlie',
+                job: 'Janitor'
+            },
+            {
+                name: 'Mac',
+                job: 'Bouncer'
+            },
+            {
+                name: 'Dee',
+                job: 'Aspiring actress'
+            },
+            {
+                name: 'Dennis',
+                job: 'Bartender'
+            },
+        ],
+    }
+    
+    removeCharacter = (index) => {
+        const {characters} = this.state
+
+        this.setState({
+            characters: characters.filter((character,i) => {
+                return i !== index
+            }),
+        })
+    }
+
     render() {
-      return (
-        <div className="App">
-          <h1>Hello, React!</h1>
-        </div>
-      )
+        const {characters} = this.state
+        
+        return (
+            <div className="container">
+            <Table characterData={characters} removeCharacter={this.removeCharacter}/>
+            </div>
+        )
     }
 }
 
--- src/Table.js
+++ src/Table.js
@@ -1,36 +1,40 @@
 import React, {Component} from 'react'
 
-class Table extends Component {
-  render() {
+const TableHeader = () => {
     return (
-      <table>
-        <thead>
-          <tr>
-            <th>Name</th>
-            <th>Job</th>
-          </tr>
-        </thead>
-        <tbody>
-          <tr>
-            <td>Charlie</td>
-            <td>Janitor</td>
-          </tr>
-          <tr>
-            <td>Mac</td>
-            <td>Bouncer</td>
-          </tr>
-          <tr>
-            <td>Dee</td>
-            <td>Aspiring actress</td>
-          </tr>
-          <tr>
-            <td>Dennis</td>
-            <td>Bartender</td>
-          </tr>
-        </tbody>
-      </table>
+      <thead>
+        <tr>
+          <th>Name</th>
+          <th>Job</th>
+        </tr>
+      </thead>
     )
   }
-}
+
+  const TableBody = (props) => {
+      const rows = props.characterData.map((row,index) => {
+          return (
+              <tr key={index}>
+                  <td>{row.name}</td>
+                  <td>{row.job}</td>
+                  <td>
+                      <button onclick={() => props.removeCharacter(index)}>Delete</button>
+                  </td>
+              </tr>
+            )
+        })
+    return <tbody>{rows}</tbody>
+  }
+
+  const Table = (props) => {
+        const {characterData, removeCharacter} = props
+      
+        return (
+            <table>
+            <TableHeader />
+            <TableBody characterData={characterData} removeCharacter={removeCharacter}/>
+            </table>
+        )
+    }   
 
 export default Table
